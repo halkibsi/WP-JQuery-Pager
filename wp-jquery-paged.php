6e21b35c2b4cfabd199bb488805d015daf84407a
Eric Amundson <eric@ivycat.com>
2013-02-22 11:54:11 -0800

    Added license file, updated readme.txt, new screenshot
    Signed-off-by: Eric Amundson <eric@ivycat.com>

--- wp-jquery-paged.php
+++ wp-jquery-paged.php
@@ -5,9 +5,28 @@
 *   Description: Uses the built-in WP gallery to creates a book with turnable pages.
 *   Author: IvyCat Web Services
 *   Author URI: http://www.ivycat.com
-*   Version: 1.03
-*   License: GPLv3
-**/
+*   Version: 1.4.0
+*   License: GNU General Public License v2.0
+*   License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ 
+ ------------------------------------------------------------------------
+	WP jQuery Pager, Copyright 2013 IvyCat, Inc. (admins@ivycat.com)
+	
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+ **/
 
 if ( ! function_exists( 'add_action' ) )
 	wp_die( 'You are trying to access this file in a manner not allowed.', 'Direct Access Forbidden', array( 'response' => '403' ) );
